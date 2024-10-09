971a75243d2f9c1c1f1d97b1f710baab30fec818
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-10-09 19:04:37 +0700

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -8,7 +8,7 @@ def calyearpillar(day,month,year) :
       month1 = str(0)+str(month) 
   year1 = int(year)-543
   yearraw = int(str(year1)+str(month1)+str(day1))
-  if yearraw - int(str(year1)+str(02)+str(04)) >= 0:
+  if yearraw - int(str(year1)+str('02')+str('04')) >= 0:
       yeartocal =   yearraw  
   else :
       yeartocal =   yearraw-1
