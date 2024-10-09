13dadbcb7f4e688e2e057458f7059cb45f2df764
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-10-09 20:12:09 +0700

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,12 +3,13 @@ dictofmonth = {"01":"มกราคม", "02":"กุมภาพันธ์",
      "07":"กรกฎาคม", "08":"สิงหาคม", "09":"กันยายน", "10":"ตุลาคม", "11":"พฤศจิกายน", "12":"ธันวาคม"} 
 day1 = '0'
 def calyearpillar(day,month,year) :
+     global day1
   if len(day) == 1:
       day1 = str(0)+str(day)
   
   year1 = int(year)-543
   month1= [k for k,v in dictofmonth.items() if v == month]
-  yearraw = int(str(year1)+str(month1)+str(day1))
+  yearraw = int(str(year1)+str(*month1)+str(day1))
   if yearraw - int(str(year1)+str('02')+str('04')) >= 0:
       yeartocal =   yearraw  
   else :
