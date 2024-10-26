0f817d700077c87153ff8b4772f5ff7785bb8812
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-10-26 19:44:13 +0700

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -43,7 +43,7 @@ def calyearpillar(date,month,year):
                      "gui":"https://img2.pic.in.th/pic/gui.jpg"
                       }
   
-  a = dictofpichiddenstem[dictofyear[int(year)%12]]
+  a = dictofearthlybranch[dictofyear[int(year)%12]]
   b = dictofyearelement[int(str(year)[-1])]  
   c = dictofpictelement[dictofyearelement[int(str(year)[-1])]]
   df = pd.DataFrame(
