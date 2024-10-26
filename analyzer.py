40975440ad8c65f6d3354100971804072c50c3e9
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-10-26 17:18:18 +0700

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,17 +1,46 @@
-listofyearpillar = []
-dictofmonth = {"01":"มกราคม", "02":"กุมภาพันธ์", "03":"มีนาคม", "04":"เมษายน", "05":"พฤษภาคม", "06":"มิถุนายน",
-     "07":"กรกฎาคม", "08":"สิงหาคม", "09":"กันยายน", "10":"ตุลาคม", "11":"พฤศจิกายน", "12":"ธันวาคม"} 
-day1 = '0'
-def calyearpillar(day,month,year) :
-     global day1
-  if len(day) == 1:
-      day1 = str(0)+str(day)
-      year1 = int(year)-543
-      month1= [k for k,v in dictofmonth.items() if v == month]
-      yearraw = int(str(year1)+str(*month1)+str(day1))
-      if yearraw - int(str(year1)+str('02')+str('04')) >= 0:
-           yeartocal =   yearraw  
-      else :
-           yeartocal =   yearraw-1
-      yearpillar = (yeartocal%60)-3
-  return  yearpillar
+dictofyear= {7:"rat",8:"ox" ,9:"tiger" ,10:"rabbit" ,11:"dragon",0:"snake" ,1:"horse" ,2:"goat" ,3:"monkey" ,4:"rooster",5:"dog" , 6:"pig" }
+dictofyearelement = {7:"jia",8:"yi",9:"bing",0:"ding",1:"wu",2:"ji",3:"geng",4:"xin",5:"ren",6:"gui"}
+dictofearthlybranch =  {"rat":"https://img5.pic.in.th/file/secure-sv1/rat.jpg",
+                         "ox":"https://img2.pic.in.th/pic/ox.jpg",
+                         "tiger":"https://img2.pic.in.th/pic/tiger.jpg",
+                        "rabbit":"https://img5.pic.in.th/file/secure-sv1/rabb.jpg",
+                        "dragon":"https://img5.pic.in.th/file/secure-sv1/dragon.jpg",
+                        "snake":"https://img5.pic.in.th/file/secure-sv1/snakebc3e231e3c143f8d.jpg",
+                        "horse":"https://img2.pic.in.th/pic/horse.jpg",
+                       "goat":"https://img5.pic.in.th/file/secure-sv1/wei.jpg",
+                     "monkey":"https://img5.pic.in.th/file/secure-sv1/monkey.jpg",
+                    "rooster":"https://img5.pic.in.th/file/secure-sv1/chick.jpg",
+                        "dog":"https://img2.pic.in.th/pic/dog8c608a2c4b6d5657.jpg",
+                        "pig":"https://img2.pic.in.th/pic/pig.jpg"
+                        }
+
+dictofpichiddenstem =  {"rat":"https://img5.pic.in.th/file/secure-sv1/hiddenrat.jpg",
+                         "ox":"https://img5.pic.in.th/file/secure-sv1/hiddenox.jpg",
+                         "tiger":"https://img5.pic.in.th/file/secure-sv1/hiddentiger.jpg",
+                     "rabbit":"https://img5.pic.in.th/file/secure-sv1/hiddenrabb.jpg",
+                      "dragon":"https://img5.pic.in.th/file/secure-sv1/hiddendragon.jpg",
+                      "snake":"https://img2.pic.in.th/pic/hiddensnake.jpg",
+                      "horse":"https://img2.pic.in.th/pic/hiddenhorse.jpg",
+                      "goat":"https://img2.pic.in.th/pic/hiddengoat.jpg",
+                   "monkey":"https://img5.pic.in.th/file/secure-sv1/hiddenmonkey.jpg",
+                      "rooster":"https://img5.pic.in.th/file/secure-sv1/hiddenchick.jpg",
+                      "dog":"https://img5.pic.in.th/file/secure-sv1/hiddendog.jpg",
+                      "pig":"https://img5.pic.in.th/file/secure-sv1/hiddenpig.jpg"
+                      }
+dictofpictelement = {"jia":"https://img5.pic.in.th/file/secure-sv1/jia.jpg",
+                      "yi":"https://img5.pic.in.th/file/secure-sv1/yi.jpg",
+                    "bing":"https://img5.pic.in.th/file/secure-sv1/bing.jpg",
+                    "ding":"https://img2.pic.in.th/pic/ding.jpg",
+                      "wu":"https://img5.pic.in.th/file/secure-sv1/wu.jpg",
+                      "ji":"https://img5.pic.in.th/file/secure-sv1/ji.jpg",
+                    "geng":"https://img5.pic.in.th/file/secure-sv1/geng.jpg",
+                     "xin":"https://img2.pic.in.th/pic/xin.jpg",
+                     "ren":"https://img2.pic.in.th/pic/ren.jpg",
+                     "gui":"https://img2.pic.in.th/pic/gui.jpg"
+                      }
+
+
+a =int(input())
+print(dictofyear[a%12])
+print(dictofyearelement[int(str(a)[-1])])
+print(dictofpictelement[dictofyearelement[int(str(a)[-1])]])
