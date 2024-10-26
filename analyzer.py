e2e15ec6734c881c8bedc94dea0488407f3b3832
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2024-10-26 19:47:51 +0700

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -51,7 +51,7 @@ def calyearpillar(date,month,year):
              "ยาม": ["A", "B"],
              "วัน": ["wer", "asc"],
              "เดือน": ['ab', 'bc'],
-            "ปี": [st.image(c),  st.image(a) ]
+            "ปี": [st.image(c),  st.image(a) ],
            }
                   )
   
